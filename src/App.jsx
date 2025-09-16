77ecde94c9818dfbb501db661d3253c842f007b4
cyber-bro001 <okwuwavictor4@gmail.com>
2025-09-16 23:50:12 +0100

    added vercel analytical tool

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react";
 import Home from './pages/Home';
 import ShopSection from './pages/ShopSection';
 import About from './pages/About';
@@ -19,6 +20,7 @@ export default function App() {
       </Routes>
         <ToTopButton />
         <Footer />
+        <Analytics />
     </Router>
   );
 }
